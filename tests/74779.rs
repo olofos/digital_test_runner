3e4e63867bbc3798b98f610365354bb5fba362a7
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-06-07 20:14:03 +0200

    Rename test

--- tests/74779.rs
+++ tests/74779.rs
@@ -4,10 +4,10 @@ use digital_test_runner::{
 };
 
 #[test]
-fn test() {
+fn load_file() {
     let input =
         std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/74779.dig"))
             .unwrap();
     let dig: DigFile = parse(&input).unwrap();
-    let _test_case = TestCase::try_from_static_dig(&dig, 0).unwrap();
+    let _ = TestCase::try_from_static_dig(&dig, 0).unwrap();
 }
