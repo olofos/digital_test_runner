59c9cdf93a1b5d4f841d323eedae497d638bcac1
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-06-16 00:08:07 +0200

    Move Cursor to util

--- examples/static.rs
+++ examples/static.rs
@@ -1,45 +1,7 @@
 use digital_test_runner::{dig, TestCase};
-use std::io::{prelude::*, BufReader};
+use std::io::Write;
 
 mod util;
-
-#[derive(Debug)]
-struct Cursor<T> {
-    reader: BufReader<T>,
-    line: String,
-    index: usize,
-}
-
-impl<T: Read> Cursor<T> {
-    pub fn new(reader: T) -> Self {
-        Self {
-            reader: std::io::BufReader::new(reader),
-            line: String::new(),
-            index: 0,
-        }
-    }
-
-    pub fn grab(&mut self, len: impl Into<usize>) -> anyhow::Result<i64> {
-        if self.index >= self.line.len() {
-            self.line.clear();
-            if self.reader.read_line(&mut self.line)? == 0 {
-                anyhow::bail!("Unexpected end of file")
-            };
-            if self.line.ends_with('\n') {
-                self.line.pop();
-            }
-            self.index = 0;
-        }
-        let len = len.into();
-        if self.line.len() < self.index + len {
-            Err(anyhow::anyhow!("Not enough data in line"))
-        } else {
-            let s = &self.line[self.index..(self.index + len)];
-            self.index += len;
-            Ok(i64::from_str_radix(s, 2)?)
-        }
-    }
-}
 
 fn main() -> anyhow::Result<()> {
     let path = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/Counter.dig");
@@ -54,7 +16,7 @@ fn main() -> anyhow::Result<()> {
         .spawn()?;
 
     let mut stdin = child.stdin.take().unwrap();
-    let mut cursor = Cursor::new(child.stdout.take().unwrap());
+    let mut cursor = util::Cursor::new(child.stdout.take().unwrap());
 
     let mut error_count = 0;
     for row in &test_case {
--- examples/util/mod.rs
+++ examples/util/mod.rs
@@ -1,3 +1,4 @@
+use std::io::{BufRead, BufReader, Read};
 use std::{path::PathBuf, process::Command};
 
 pub fn compile_verilog(name: &str, sources: &[&str]) -> anyhow::Result<PathBuf> {
@@ -21,5 +22,43 @@ pub fn compile_verilog(name: &str, sources: &[&str]) -> anyhow::Result<PathBuf> 
         Ok(output_path)
     } else {
         Err(anyhow::anyhow!("Error while executing iverilog"))
+    }
+}
+
+#[derive(Debug)]
+pub struct Cursor<T> {
+    reader: BufReader<T>,
+    line: String,
+    index: usize,
+}
+
+impl<T: Read> Cursor<T> {
+    pub fn new(reader: T) -> Self {
+        Self {
+            reader: std::io::BufReader::new(reader),
+            line: String::new(),
+            index: 0,
+        }
+    }
+
+    pub fn grab(&mut self, len: impl Into<usize>) -> anyhow::Result<i64> {
+        if self.index >= self.line.len() {
+            self.line.clear();
+            if self.reader.read_line(&mut self.line)? == 0 {
+                anyhow::bail!("Unexpected end of file")
+            };
+            if self.line.ends_with('\n') {
+                self.line.pop();
+            }
+            self.index = 0;
+        }
+        let len = len.into();
+        if self.line.len() < self.index + len {
+            Err(anyhow::anyhow!("Not enough data in line"))
+        } else {
+            let s = &self.line[self.index..(self.index + len)];
+            self.index += len;
+            Ok(i64::from_str_radix(s, 2)?)
+        }
     }
 }
