40cf25ed2904bea2daee8a8ccc11b1283e409fce
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-07-14 21:41:58 +0200

    Add documentation to RunError

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,12 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
+/// Error returned from DataRowIterator
 pub enum RunError<T> {
+    /// Driver error
     #[error("Driver error")]
     Driver(#[from] T),
+    /// Runtime error
     #[error("Runtime error")]
     Runtime(anyhow::Error),
 }
